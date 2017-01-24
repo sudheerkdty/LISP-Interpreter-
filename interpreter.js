c093614055491423d854f5bd334ec5abddc0ed6e
sudheerkdty <sudheerkdty@gmail.com>
2017-01-24 18:05:04 +0530

    global envt

--- interpreter.js
+++ interpreter.js
@@ -1,8 +1,54 @@
+function add(){
+    return arguments[0] + arguments[1];
+}
+
+function sub(){
+    return arguments[0] - arguments[1];
+}
+
+function mul(){
+    return arguments[0] * arguments[1];
+}
+
+function div(){
+    if (arguments[1] == 0){
+        throw "Division by zero is undefined";
+    }
+    return arguments[0] / arguments[1];
+}
+
+global_env = {'+': add,'-': sub, '*': mul, '/' : div};
+
+function eval(x, env ){
+    env = env || global_env;
+    if (typeof(x) == 'string'){
+        //console.log("x is "+ x);
+        return env[x];
+    }
+    else if (!(x instanceof Array)){
+        //console.log("constants"+ x);
+        return x;
+    }
+    else{
+        //console.log("operator"+ x[0]);
+        var  proc =eval(x[0],env);//x[0], env);
+        //console.log("x="+x);//typeof(proc));
+        var args = [];
+        xnew = x.slice(1);
+        for (var i in xnew){
+            //console.log("x(i):"+typeof(xnew[i]));
+            args.push(eval(xnew[i], env));
+        }
+        //console.log("args:"+args);
+        //console.log("type of proc:"+typeof(proc));
+        return proc.apply(this,args);
+    }
+}
+console.log(eval('x'));
 function tokenize(chars){
     var newchar = chars.replace(/\(/g,'( ').replace(/\)/g,' )')
     return newchar.split(/ +/).slice(0,newchar.length);
 };
-
 function read_from_tokens(tokens){
     var len = tokens.length;
     //console.log("tokens.length = "+ len);
@@ -44,13 +90,7 @@ function parse(program){
     return read_from_tokens(tokenize(program));
     }
 
-console.log(parse("4"));
-console.log(parse("1.23"));
-console.log(parse("(+ 1 2)"));
-console.log(parse("(+ 1 2.3 45)"));
-
-//nested expressions
-
-console.log(parse("(+ 1 2 (* 3 4))"));
-console.log(parse("(+ 1 2 (* 3 (- 5 4)))"));
-
+console.log("Eval************");
+console.log(eval(parse("(+ 2 (* 3 4))")));
+console.log(eval(parse("(+ 2 (* 3 (- 5 3)))")));
+console.log(eval(parse("4")));
