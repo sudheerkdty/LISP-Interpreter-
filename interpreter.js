a924c59bd90411d453f929dbbbbeaf89b6be034c
sudheerkdty <sudheerkdty@gmail.com>
2017-01-24 18:09:51 +0530

    evaluate conditions

--- interpreter.js
+++ interpreter.js
@@ -16,8 +16,19 @@ function div(){
     }
     return arguments[0] / arguments[1];
 }
+function gt(){
+    return arguments[0] > arguments[1];
+}
 
-global_env = {'+': add,'-': sub, '*': mul, '/' : div};
+function lt(){
+    return arguments[0] < arguments[1];
+}
+
+function eq(){
+    return arguments[0] == arguments[1];
+}
+
+global_env = {'+': add,'-': sub, '*': mul, '/' : div,'>': gt,'<' : lt, '=' : eq };
 
 function eval(x, env ){
     env = env || global_env;
@@ -27,10 +38,21 @@ function eval(x, env ){
     else if (!(x instanceof Array)){
         return x;
     }
-    else if (x[0] == 'define'){
+    else if (x[0] == 'define'){//to implement variables
         variable = x[1];
         exp = x[2];
         env[variable] = eval(exp, env);
+    }
+    else if (x[0] == 'if'){//implement if test then_expr else expr
+        test = x[1];
+        then_expr = x[2];
+        else_expr = x[3];
+        if (eval(test, env)){
+            return eval(then_expr, env);
+            }
+        else{
+            return eval(else_expr, env);
+            }
     }
     else{
         var  proc =eval(x[0],env);
@@ -89,6 +111,6 @@ pgm1 = '(define x 10)';
 eval(parse(pgm1));
 pgm2 = '(define y (* 3 4))';
 eval(parse(pgm2));
-pgm3 = '(+ x y)'
+pgm3 ='(if (< x  y) (+ 1 2) (- 5 6))';
 console.log(pgm1+"\n"+pgm2+"\n"+pgm3);
 console.log(eval(parse(pgm3)));
